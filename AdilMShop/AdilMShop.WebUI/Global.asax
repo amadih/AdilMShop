ebb87f48486a208cb1915e09fce2c43331fd9171
adil madih <adilmadih1@gmail.com>
2018-04-12 21:42:28 -0500

    Initial Set up

---
+++ AdilMShop/AdilMShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdilMShop.WebUI.MvcApplication" Language="C#" %>
---
+++ AdilMShop/AdilMShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdilMShop.WebUI.Startup))]
+namespace AdilMShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
